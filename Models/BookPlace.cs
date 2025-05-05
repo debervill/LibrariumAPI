60ff3383eadb4e19e4f0fbd20dfc93829bad5726
Danamir <alexandr.salny@yandex.ru>
2025-05-05 14:29:26 +0300

    Create Model table

--- Models/BookPlace.cs
+++ Models/BookPlace.cs
@@ -1,7 +1,7 @@
 ﻿namespace LibrariumAPI.Models
 {
     public class BookPlace
-    {
+    {   
         public int Id { get; set; }
         public int Quantity { get; set; }
         public int ShelfNumber { get; set; }
